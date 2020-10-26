c26cb2bda9e0998237fcaf04525e703b067e266d
Albert Shymanel <albertshymanel@gmail.com>
2020-10-27 02:17:50 +0300

    Liquibase database

--- src/main/java/by/shymanel/springlab/SpringlabApplication.java
+++ src/main/java/by/shymanel/springlab/SpringlabApplication.java
@@ -2,8 +2,13 @@ package by.shymanel.springlab;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+
 
 @SpringBootApplication
+@EntityScan(basePackages = {"by.shymanel.springlab.model"} )
+@EnableJpaRepositories(basePackages = {"by.shymanel.springlab.repository"})
 public class SpringlabApplication {
 
     public static void main(String[] args) {
